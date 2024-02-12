833d9633bf5f2d5e99ee07fe274bb373dfffd345
Brian Henson <castlenthesky@gmail.com>
2024-02-12 07:01:45 -0700

    firebase refactor

--- src/services/firebase/FirebaseClient.ts
+++ src/services/firebase/FirebaseClient.ts
@@ -1,1 +1,38 @@
+import config from "../../../src/config";
 
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+
+const firebaseApp = initializeApp(config.firebase.options);
+const firebaseAuth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+
+export class FirebaseClient {
+  private static instance: FirebaseClient;
+
+  // Make the constructor private to prevent direct construction calls with the `new` operator.
+  private constructor() {}
+
+  // The static method that controls access to the singleton instance.
+  public static getInstance(): FirebaseClient {
+    if (!FirebaseClient.instance) {
+      FirebaseClient.instance = new FirebaseClient();
+    }
+    return FirebaseClient.instance;
+  }
+
+  // Method to write an object to a specified collection
+  async writeToCollection(collectionName: string, data: object): Promise<void> {
+    try {
+      const docRef = await addDoc(collection(db, collectionName), data);
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+}
+
+const firebase = FirebaseClient.getInstance();
+
+export { firebase };
